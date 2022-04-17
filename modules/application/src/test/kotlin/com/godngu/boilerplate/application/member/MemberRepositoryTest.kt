80cff499b7cf70976e75f7f55b8061cdbf479e95
kil <godngu@gmail.com>
2022-04-18 08:34:53 +0900

    모듈별 테스트 로직 동작 확인 -> yml파일 설정

--- modules/application/src/test/kotlin/com/godngu/boilerplate/application/member/MemberRepositoryTest.kt
+++ modules/application/src/test/kotlin/com/godngu/boilerplate/application/member/MemberRepositoryTest.kt
@@ -6,9 +6,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("domain-local", "infra-local")
 @SpringBootTest
 class MemberRepositoryTest {
 
