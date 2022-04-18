b5cc0180ea5a63bab3ca6ec10592fc8694afa28e
kil <godngu@gmail.com>
2022-04-19 06:24:16 +0900

    페이징 테스트로직 주석처리

--- modules/application/src/main/kotlin/com/godngu/boilerplate/application/member/MemberFindService.kt
+++ modules/application/src/main/kotlin/com/godngu/boilerplate/application/member/MemberFindService.kt
@@ -18,7 +18,7 @@ class MemberFindService(
     // TODO : 테스트 로직 삭제해야 함
     @PostConstruct
     fun setUp() {
-        saveMembersForTest()
+//        saveMembersForTest()
     }
 
     @Transactional(readOnly = true)
